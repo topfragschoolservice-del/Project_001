6dc8a2ffd9cf4887393fe1a82b7a701d92327604
garusingheehci <chenithithsara@gmail.com>
2026-03-29 20:26:19 +0530

    Create EventStore.js

---
+++ src/js/models/EventStore.js
@@ -0,0 +1,17 @@
+export class EventStore {
+  constructor() {
+    this.items = [
+      this.makeEvent("System initialized", "info"),
+    ];
+  }
+
+  makeEvent(message, type = "info") {
+    const time = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+    return { id: crypto.randomUUID(), message, type, time };
+  }
+
+  push(message, type = "info") {
+    this.items.unshift(this.makeEvent(message, type));
+    this.items = this.items.slice(0, 18);
+  }
+}
